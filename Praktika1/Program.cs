f79fb5a15e66c3b841a9d2de470ece9ae5d2a9f9
name <brylek2288@mail.ru>
2025-03-17 10:36:20 +0300

    Создан класс

--- Praktika1/Program.cs
+++ Praktika1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Praktika1
+{
+    public class Река
+    {
+        public string Название { get; set; }
+        public double ПлощадьБассейна { get; set; }
+        public double Протяжённость { get; set; }
+
+    }
+}
